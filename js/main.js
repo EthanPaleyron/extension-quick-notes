103a588c19e51a68b8469b6a9af3c7fec8cf5f51
EthanPaleyron <epaleyron@edenschool.fr>
2024-02-09 00:11:32 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const notepadList = document.querySelector("#notepadList");
+const inputTitle = document.querySelector("#inputTitle");
+const inputText = document.querySelector("#inputText");
+
+// Rajoute un bloc de note automatiquement s'il n'y en a pas encore
+if (notepadList.children.length === 0) {
+  notepadList.innerHTML = `<li><button></button></li>`;
+}
+
+// Ajoute un identifiant unique à chaque élément de la liste
+notepadList.querySelectorAll("li button").forEach((notepad, i) => {
+  notepad.setAttribute("id", "notepad_" + i);
+  const inputHiddenTitle = document.createElement("input");
+  inputHiddenTitle.type = "hidden";
+  inputHiddenTitle.id = "title_" + i;
+  const inputHiddenText = document.createElement("input");
+  inputHiddenText.type = "hidden";
+  inputHiddenText.id = "text_" + i;
+  const notepadTitle = document.createElement("span");
+  notepadTitle.id = "notepadTitle_" + i;
+  notepadTitle.textContent = "Untitled";
+  notepad.appendChild(inputHiddenTitle);
+  notepad.appendChild(inputHiddenText);
+  notepad.appendChild(notepadTitle);
+});
+
+// copie les valeurs des champs cachés correspondants dans les champs d'entrée
+let idNotepad = 0;
+notepadList.querySelectorAll("li button").forEach((notepad, i) => {
+  notepad.addEventListener("click", () => {
+    idNotepad = i;
+    const inputHiddenTitle = notepad.querySelector("#title_" + i);
+    const inputHiddenText = notepad.querySelector("#text_" + i);
+    inputTitle.value = inputHiddenTitle.value;
+    inputText.value = inputHiddenText.value;
+  });
+});
+
+
+document.querySelector("input").addEventListener("input", () => {
+  const title = document.querySelector("#title_" + idNotepad);
+  const text = document.querySelector("#text_" + idNotepad);
+  const notepadTitle = document.querySelector("#notepadTitle_" + idNotepad);
+  title.value = inputTitle.value;
+  text.value = inputText.value;
+  // Limite de carateres
+  if (inputTitle.value.length > 20) {
+    inputTitle.value = inputTitle.value.slice(0, 20);
+  }
+  if (inputTitle.value.trim() === "") {
+    notepadTitle.textContent = "Untitled";
+  } else {
+    notepadTitle.textContent = inputTitle.value;
+  }
+});
